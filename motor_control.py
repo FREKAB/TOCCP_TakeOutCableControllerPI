c2626bc4b0d4fa368ce6ccd378b92b836272b31e
FREKAB-Niklas <niklas@frekab.se>
2024-08-12 10:42:10 +0200

    thread

--- motor_control.py
+++ motor_control.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 import paho.mqtt.client as mqtt
+import threading
 
 # GPIO setup
 PUL = 17
@@ -10,9 +11,17 @@ steps = 1600
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(PUL, GPIO.OUT)
 GPIO.setup(DIR, GPIO.OUT)
-GPIO.output(DIR, GPIO.HIGH)
 
-# MQTT setup
+def run_motor():
+    GPIO.output(DIR, GPIO.HIGH)  # Set direction
+    for i in range(steps):
+        GPIO.output(PUL, GPIO.HIGH)
+        time.sleep(0.0001)
+        GPIO.output(PUL, GPIO.LOW)
+        time.sleep(0.0001)
+    print("Motor run complete")
+    # Don't call GPIO.cleanup() here if you want to be able to run the motor again
+
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
     client.subscribe("motor/control")
@@ -21,22 +30,16 @@ def on_message(client, userdata, msg):
     if msg.topic == "motor/control":
         command = msg.payload.decode()
         if command == "RUN":
-            run_motor()
+            print("RUN command received")
+            # Run the motor in a separate thread to avoid blocking
+            threading.Thread(target=run_motor).start()
 
-def run_motor():
-    for i in range(steps):
-        GPIO.output(PUL, GPIO.HIGH)
-        time.sleep(0.0001)
-        GPIO.output(PUL, GPIO.LOW)
-        time.sleep(0.0001)
-    print("Motor run complete")
-    GPIO.cleanup()
-
-# Connect to the MQTT broker
-mqtt_broker_ip = "192.168.10.9"  # Replace with your actual broker IP
+# MQTT setup
+mqtt_broker_ip = "192.168.10.9"  # Replace with your broker IP
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
 client.connect(mqtt_broker_ip, 1883, 60)
-client.loop_forever()
+client.loop_forever()  # Keep listening for incoming messages
+
