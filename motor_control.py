e743dffeaf5777656b491d876c2ed320a95472d7
FREKAB-Niklas <niklas@frekab.se>
2024-09-23 10:42:10 +0200

    t

--- motor_control.py
+++ motor_control.py
@@ -41,7 +41,6 @@ def reset_motor_driver():
 
 def run_motor(direction, start_speed=0.001, max_speed=0.0001, accel_steps=1600):
     GPIO.output(DIR, direction)
-    GPIO.output(ENABLE_PIN, GPIO.HIGH)  # Enable the motor
     
     # Acceleration phase
     for i in range(accel_steps):
