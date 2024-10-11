569d43eaeebd644c8aece2db8dace4b0b5bf33fc
FREKAB-Niklas <niklas@frekab.se>
2024-10-11 12:54:21 +0200

    Update motor_control.py

--- motor_control.py
+++ motor_control.py
@@ -240,9 +240,9 @@ def motor_control_loop():
 
             GPIO.output(DIR, GPIO.HIGH)
             GPIO.output(PUL, GPIO.LOW)
-            time.sleep(motor_speed)
+            time.sleep(0.0001)
             GPIO.output(PUL, GPIO.HIGH)
-            time.sleep(motor_speed)
+            time.sleep(0.0001)
 
             # Timeout check for manual mode
             if manual_mode and time.time() - last_manual_run_time > timeout_threshold:
